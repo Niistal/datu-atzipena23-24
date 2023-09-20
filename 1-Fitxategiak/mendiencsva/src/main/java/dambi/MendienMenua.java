8db19cdbbaafb3ff20c6307e8506e68d100fe04e
Niistal <114486230+Niistal@users.noreply.github.com>
2023-09-20 12:04:20 +0200

    mendi taula

---
+++ 1-Fitxategiak/mendiencsva/src/main/java/dambi/MendienMenua.java
@@ -0,0 +1,16 @@
+package dambi;
+
+public class MendienMenua {
+    
+    public static void main(String[] args) {
+        System.out.println("     Mendien Menua   ");
+        System.out.println("================================");
+        System.out.println("1. Mendien zerrenda ikusi  "); //(taula formatuan)
+        System.out.println("2. Mendirik altuena bistaratu");
+        System.out.println("3. Mendiak esportatu "); //(Araba.csv,Bizkaia.csv, Gipuzkoa.csv)
+        System.out.println("Irten");
+
+
+
+    }
+}
