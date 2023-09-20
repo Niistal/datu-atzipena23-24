7f70e5674b45af325a3e23ff880ee6c1a1d6f7eb
Niistal <114486230+Niistal@users.noreply.github.com>
2023-09-20 11:43:59 +0200

    aldaketak

--- 1-Fitxategiak/mendiencsva/src/main/java/dambi/App.java
+++ 1-Fitxategiak/mendiencsva/src/main/java/dambi/App.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package dambi;
 
 /**
@@ -12,18 +11,3 @@ public class App
         System.out.println( "Hello World!" );
     }
 }
-=======
-package dambi;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
->>>>>>> fcc7d9cae9cea005c23777acebed5f12223dd219
--- 1-Fitxategiak/mendiencsva/src/test/java/dambi/AppTest.java
+++ 1-Fitxategiak/mendiencsva/src/test/java/dambi/AppTest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package dambi;
 
 import static org.junit.Assert.assertTrue;
@@ -19,25 +18,3 @@ public class AppTest
         assertTrue( true );
     }
 }
-=======
-package dambi;
-
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
-}
->>>>>>> fcc7d9cae9cea005c23777acebed5f12223dd219
