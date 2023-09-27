eff1302770e3b1d752dc5429573224a43e81a963
Niistal <114486230+Niistal@users.noreply.github.com>
2023-09-27 12:25:52 +0200

    mendiak

---
+++ 2-XML Fitxategiak/mendiakxml/src/main/java/dambi/unmarshal/GipuzkoakoMendiak.java
@@ -0,0 +1,11 @@
+package dambi.unmarshal ;
+import java.io.File;
+
+import dambi.mendiak.HiruMendiSortu;
+import dambi.mendiak.MendiBatSortu;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
+public class GipuzkoakoMendiak {
+    
+}
---
+++ 2-XML Fitxategiak/mendiakxml/src/main/java/dambi/unmarshal/MendiakAltueraOinatan.java
@@ -0,0 +1,9 @@
+package dambi.unmarshal;
+import dambi.mendiak.HiruMendiSortu;
+import dambi.mendiak.MendiBatSortu;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
+public class MendiakAltueraOinatan {
+    
+}
