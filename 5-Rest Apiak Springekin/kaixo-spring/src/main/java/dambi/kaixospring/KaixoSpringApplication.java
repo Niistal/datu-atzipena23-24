d375d30d3a4c83c8106381969f3e58375952eff6
Niistal <114486230+Niistal@users.noreply.github.com>
2023-11-21 13:08:28 +0100

    Restapiak

---
+++ 5-Rest Apiak Springekin/kaixo-spring/src/main/java/dambi/kaixospring/KaixoSpringApplication.java
@@ -0,0 +1,13 @@
+package dambi.kaixospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaixoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaixoSpringApplication.class, args);
+	}
+
+}
